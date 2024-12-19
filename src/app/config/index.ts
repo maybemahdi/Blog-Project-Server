445b248ce6c7aa9ce88ce4c0932ee3d093e1e8f3
Mahdi Hasan Mahi <mh7266391@gmail.com>
2024-12-19 20:49:02 +0600

    initiated project

---
+++ src/app/config/index.ts
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join((process.cwd(), ".env")) });
+
+export default {
+  port: process.env.PORT,
+  db_uri: process.env.DB_URI,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  node_env: process.env.NODE_ENV,
+};
